dc98c7d685015bac2409dccc964cecda87099131
Anushalatha <anushalathaaa7@gmail.com>
2025-05-16 08:37:48 +0530

    "First commit"

--- hello.DevOps/test.js
+++ hello.DevOps/test.js
@@ -1,8 +1,9 @@
 const greet=require('./app');
 if(greet()==="Hello from DevOps!"){
     console.log("Test passed");
-    ProcessingInstruction.exit(0);
+    process.exit(0);
+
 }else{
     console.log("Test failed");
-    ProcessingInstruction.exit(1);
+    process.exit(0);
 }
